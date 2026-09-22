f79fd0b5b2aef74af9c92b59d47aa98d22fef721
Isayadon <isabelliantonietti23@gmail.com>
2026-09-22 16:30:49 -0300

    adiciona programa que imprime "Slowpoke" dez vezes

---
+++ 16.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int contador;
+    contador = 0;
+    
+    while ( contador < 10 ) {
+        
+        cout << "Slowpoke" << endl;
+
+        contador = contador + 1;
+    }
+    
+    return 0;
+}
