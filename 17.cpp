2a495eeb97a82455d19dc4484880690f44d2ba86
Isayadon <isabelliantonietti23@gmail.com>
2026-09-22 16:46:56 -0300

    adiciona programa que imprime números de 1 a 10

---
+++ 17.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int contador;
+
+    contador = 1;
+
+    while (contador <= 10)
+    {
+        cout << contador << endl;
+        contador = contador + 1;
+    }
+
+    return 0;
+}
